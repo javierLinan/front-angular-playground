4f206e6f6c6a466eae35bb807a985b6b6328b5fe
Jose Javier Linan Manzaneda <javier.linmanzaneda@gmail.com>
2018-01-18 15:26:33 +0100

    Sonar scan and SonarQube projet update when the develop branch is pushed
    t003

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -1,10 +1,19 @@
+
 #!/bin/bash
 set -ev
 
 curl -sSL https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.0.1.733-linux.zip > sonar-scanner.zip
 unzip sonar-scanner.zip
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [[ "$TRAVIS_PULL_REQUEST_BRANCH" = "feature/"* ]]
+if [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
+then
+  echo 'Build and analyze develop'
+  ./sonar-scanner-3.0.1.733-linux/bin/sonar-scanner \
+    -Dproject.settings="../sonar-project.properties" \
+    -Dsonar.host.url=$SONAR_HOST_URL \
+    -Dsonar.login=$SONAR_TOKEN
+
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [[ "$TRAVIS_PULL_REQUEST_BRANCH" = "feature/"* ]]
 then
   echo 'Build feature branch'
 
